c947446a3790c42a4ec9af1abd6093eeb618a361
Supun Nethsara <supunmax663@gmail.com>
2025-09-10 08:49:41 -0700

    Laravel sanctum installed

--- tests/Feature/UserRegisterTest.php
+++ tests/Feature/UserRegisterTest.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 
 uses(RefreshDatabase::class);
 
@@ -42,4 +44,21 @@ it('fails to register a user with invalid data', function () {
         'password',
     ]);
 });
-
+it('can login a registered user ', function () {
+     User::create([
+        'name' => 'Supun Nethsara',
+        'email' => 'supun@example.com',
+        'phone' => '0771234567',
+        'role' => 'admin',
+        'password' => Hash::make('password123'),
+    ]);
+    $response = $this->postJson('/api/login',[
+        'email' => 'supun@example.com',
+        'password' => 'password123',
+    ]);
+    $response->assertStatus(200)
+        ->assertJsonStructure([
+            'user' => ['id', 'name', 'email', 'phone', 'role'],
+            'token',
+        ]);
+});
