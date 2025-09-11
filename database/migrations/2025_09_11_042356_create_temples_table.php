ec557f49879356e04c5a930250da4415e63ac214
Supun Nethsara <supunmax663@gmail.com>
2025-09-10 21:50:43 -0700

    feature(temple): implement model, controller and related process

--- database/migrations/2025_09_11_042356_create_temples_table.php
+++ database/migrations/2025_09_11_042356_create_temples_table.php
@@ -12,7 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('temples', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('location');
+            $table->string('contact', 15);
+            $table->string('currency')->default('LKR');
+            $table->date('time_zone');
             $table->timestamps();
         });
     }
